48f1195ae9961276bc9c4aab824c1ddb34df214d
Youssef Kandil <youssefkandil@aucegypt.edu>
2025-01-05 09:43:36 +0200

    adding license headers

--- fw/EF_UART.h
+++ fw/EF_UART.h
@@ -1,8 +1,24 @@
-/*! \file EF_UART.h
-    \brief C header file for UART APIs which contains the function prototypes 
-    
+/*
+	Copyright 2025 Efabless Corp.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	    www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
 */
 
+
+/*! \file EF_UART.h
+    \brief C header file for UART APIs which contains the driver structure definition.
+*/
 
 #ifndef EF_UART_H
 #define EF_UART_H
